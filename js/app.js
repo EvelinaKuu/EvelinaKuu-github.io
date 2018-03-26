aa20dc0be2aa887ff25bf97f951fcff6519ed2a0
EvelinaKuu <evelina.kuusinen@gmail.com>
2018-03-26 22:45:03 +0100

    changed the navbar and styling to fix bugs

--- js/app.js
+++ js/app.js
@@ -1,8 +1,28 @@
 function init() {
+
+
   $(document).ready(function() {
-    $('#fullpage').fullpage();
-    // anchors:['firstPage', 'secondPage', 'thirdPage']
+    $('#fullpage').fullpage({
+      anchors: ['home', 'about', 'work', 'skills', 'contact'],
+      menu: '#menu'
+      // verticalCentered: false
+    });
   });
 }
 
 init();
+
+
+//
+// function init() {
+//   $(document).ready(function(){
+//     $('.button-collapse').sideNav();
+//     $('#fullpage').fullpage({
+//       anchors: ['about', 'projects', 'skills', 'contact'],
+//       menu: '#myMenu',
+//       verticalCentered: false
+//     });
+//     $('.materialboxed').materialbox();
+//   });
+// }
+// init();
