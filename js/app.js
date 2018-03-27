7b2224cdfd0057d2e2fa7a01b4e6f55251a3ee65
EvelinaKuu <evelina.kuusinen@gmail.com>
2018-03-27 14:52:37 +0100

    styled navbar to be fixed and horizontal

--- js/app.js
+++ js/app.js
@@ -4,10 +4,12 @@ function init() {
   $(document).ready(function() {
     $('#fullpage').fullpage({
       anchors: ['home', 'about', 'work', 'skills', 'contact'],
-      menu: '#menu',
+      menu: '#myMenu',
       paddingBottom: '10px',
       scrollingSpeed: 630,
       loopBottom: true
+
+
       // autoScrolling: false
 
 
