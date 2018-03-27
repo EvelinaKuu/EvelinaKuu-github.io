d0c88b408a74bd7a545ae9f804e1070e5b811bbb
EvelinaKuu <evelina.kuusinen@gmail.com>
2018-03-27 01:04:52 +0100

    added padding to contacticons

--- js/app.js
+++ js/app.js
@@ -4,25 +4,14 @@ function init() {
   $(document).ready(function() {
     $('#fullpage').fullpage({
       anchors: ['home', 'about', 'work', 'skills', 'contact'],
-      menu: '#menu'
-      // verticalCentered: false
+      menu: '#menu',
+      paddingBottom: '10px'
+      // autoScrolling: false
+
+
     });
+
   });
 }
 
 init();
-
-
-//
-// function init() {
-//   $(document).ready(function(){
-//     $('.button-collapse').sideNav();
-//     $('#fullpage').fullpage({
-//       anchors: ['about', 'projects', 'skills', 'contact'],
-//       menu: '#myMenu',
-//       verticalCentered: false
-//     });
-//     $('.materialboxed').materialbox();
-//   });
-// }
-// init();
